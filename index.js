0a9988d61c9e1347a6cdf20e801c57c911c31d65
Pardon Ndlovu <pndlovu385@gmail.com>
2025-06-03 09:59:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose');
 const cors = require('cors');
 const path = require('path');
 const fileUpload = require('express-fileupload');
+const User = require('./models/User.model');
 require('dotenv').config();
 
 const app = express();
